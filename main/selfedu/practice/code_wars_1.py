0f6fd2704801c83e9169bcd4ceb428ab7b53c270
Evgenii Morozov <zheka1101@yandex.ru>
2021-04-02 12:22:23 +0500

    Selfeducation
    code_wars

--- main/selfedu/practice/code_wars_1.py
+++ main/selfedu/practice/code_wars_1.py
@@ -112,6 +112,146 @@ def high(string):
 #     return max(x.split(), key=lambda k: sum(ord(c) - 96 for c in k))
 
 
+# Task_8
+# Trolls are attacking your comment section!
+# A common way to deal with this situation is to remove all of the vowels from the trolls' comments,
+# neutralizing the threat.
+# Your task is to write a function that takes a string and return a new string with all vowels removed.
+# For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+# Note: for this kata y isn't considered a vowel.
+
+def disemvowel(string):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    return ''.join(list(filter(lambda x: x.lower() not in vowels, string)))
+
+
+# Task_9
+# Complete the method/function so that it converts dash/underscore delimited words into camel casing.
+# The first word within the output should be capitalized only if the original word was capitalized
+# (known as Upper Camel Case, also often referred to as Pascal case).
+#
+# Examples
+# "the-stealth-warrior" gets converted to "theStealthWarrior"
+# "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+def to_camel_case(text):
+    text = text.replace('_' and '-', ' ').split()
+    return text[0] + ''.join([text[i].capitalize() for i in range(1, len(text))])
+
+
+# Task_10
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+# The sum of these multiples is 23.
+# Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+#
+# Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative,
+# return 0(for languages that do have them)
+
+def solution_1(number):
+    return 0 if number < 0 else sum(list(filter(lambda x: x % 3 == 0 or x % 5 == 0, range(number))))
+
+
+# Task_11
+# Write a function that takes in a string of one or more words, and returns the same string,
+# but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist
+# of only letters and spaces. Spaces will be included only when more than one word is present.
+#
+# Examples:
+# spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
+# spinWords( "This is a test") => returns "This is a test"
+# spinWords( "This is another test" )=> returns "This is rehtona test"
+
+def spin_words(sentence):
+    result = list(map(lambda word: word if len(word) < 5 else word[::-1], sentence.split()))
+    return ' '.join(result)
+
+
+# Task_12
+# You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or
+# other items. We want to create the text that should be displayed next to such an item.
+# Implement a function likes :: [String] -> String, which must take in input array, containing the names of
+# people who like an item. It must return the display text as shown in the examples:
+#
+# likes([]) # must be "no one likes this"
+# likes(["Peter"]) # must be "Peter likes this"
+# likes(["Jacob", "Alex"]) # must be "Jacob and Alex like this"
+# likes(["Max", "John", "Mark"]) # must be "Max, John and Mark like this"
+# likes(["Alex", "Jacob", "Mark", "Max"]) # must be "Alex, Jacob and 2 others like this"
+# For 4 or more names, the number in and 2 others simply increases.
+
+def likes(names):
+    if len(names) == 0:
+        return 'no one likes this'
+    elif len(names) == 1:
+        return f'{names[0]} likes this'
+    elif len(names) == 2:
+        return f'{names[0]} and {names[1]} like this'
+    elif len(names) == 3:
+        return f'{names[0]}, {names[1]} and {names[2]} like this'
+    elif len(names) > 4:
+        return f'{names[0]}, {names[1]} and {len(names) - 2} others like this'
+
+
+# Task_13
+# Digital root is the recursive sum of all the digits in a number.
+# Given n, take the sum of the digits of n. If that value has more than one digit,
+# continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+#
+# Examples
+#     16  -->  1 + 6 = 7
+#    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+# 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+# 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+from functools import reduce
+
+
+def digital_root(number):
+    if len(str(number)) == 1:
+        return number
+    number = reduce(lambda res, x: res + x, map(int, str(number)))
+    return digital_root(number)
+
+
+# Task_14
+# You live in the city of Cartesia where all roads are laid out in a perfect grid.
+# You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk.
+# The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends
+# you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only
+# a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create
+# a function that will return true if the walk the app gives you will take you exactly ten minutes
+# (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+#
+# Note: you will always receive a valid array containing a random assortment of direction
+# letters ('n', 's', 'e', or 'w' only). It will never give you
+# an empty array (that's not a walk, that's standing still!).
+
+def is_valid_walk(walk):
+    if len(walk) != 10:
+        return False
+    else:
+        return True if walk.count('s') == walk.count('n') and walk.count('w') == walk.count('e') else False
+
+
+# Task_15
+# Implement the function unique_in_order which takes as argument a sequence and returns a list of items without
+# any elements with the same value next to each other and preserving the original order of elements.
+#
+# For example:
+# unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+# unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+# unique_in_order([1,2,2,3,3])       == [1,2,3]
+
+def unique_in_order(iterable):
+    indices = list(filter(lambda i: iterable[i] != iterable[i-1], range(1, len(iterable))))
+    return [iterable[0]] + [iterable[i] for i in indices] if len(iterable) != 0 else []
+
+# from itertools import groupby
+#
+# def unique_in_order(iterable):
+#     return [k for (k, _) in groupby(iterable)]
+
+
 def main():
     # Task_1
     # print(count_bits(1234))
@@ -125,7 +265,22 @@ def main():
     # Task_5
     # print(open_or_senior([[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]))
     # Task_7
-    print(high('man i need a taxi up to ubud'))
+    # print(high('man i need a taxi up to ubud'))
+    # Task_8
+    # print(disemvowel("This website is for losers LOL!"))
+    # Task_9
+    print(to_camel_case("the-stealth-warrior"))
+    # Task_10
+    # print(solution_1(12))
+    # Task_11
+    # print(spin_words("Hey fellow warriors"))
+    # Task_12
+    # print(digital_root(16))
+    # Task_14
+    # print(is_valid_walk(['n','s','n','s','n','s','n','s','n','s']))
+    # print(is_valid_walk(['w','e','w','e','w','e','w','e','w','e','w','e']))
+    # Task_15
+    print(unique_in_order('AAAABBBCCDAABBB'))
 
 
 if __name__ == '__main__':
