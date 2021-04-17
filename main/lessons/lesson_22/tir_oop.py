d5eb6af53af6cc1890d9819e8855117397e4a3f3
EvgeniiMorozov <emorozov1101@gmail.com>
2021-04-17 13:18:38 +0500

    Урок 17.04.2021

--- main/lessons/lesson_22/tir_oop.py
+++ main/lessons/lesson_22/tir_oop.py
@@ -20,6 +20,9 @@ class Game:
         pygame.display.set_caption(self.caption)
         pygame.mouse.set_visible(False)
         self.clock = pygame.time.Clock()
+        self.player = []
+        self.targets = []
+        self.hud_obj = []
         self.game_objects = []
         self.mouse_handlers = defaultdict(list)  # dct = {"нажали на мышку": [obj1, obj2],
         # "передвинули мышку": [handle_mouse, obj2]} - вместо obj будут функции-обработчики событий
@@ -32,6 +35,8 @@ class Game:
 
         while True:
             self.handle_events()
+
+            self.game_objects = [*self.targets, *self.hud_obj, *self.player]
 
             self.surface.fill(self.background)
             self.update()
@@ -66,11 +71,16 @@ class Game:
             # MOUSEMOTION и MOUSEBUTTONDOWN разные аттрибуты.
             if event.type == pygame.MOUSEMOTION:
                 for handler in self.mouse_handlers[event.type]:
-                    handler(event.type, event.pos, event.rel)
+                    handler(event.type, event.pos)
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            # мой вариант
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+            #     for handler in self.mouse_handlers[event.type]:
+            #         handler(event.type, event.pos, event.button)
+
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 for handler in self.mouse_handlers[event.type]:
-                    handler(event.type, event.pos, event.button)
+                    handler(event.type, event.pos)
 
 
 class Tir(Game):
@@ -84,6 +94,7 @@ class Tir(Game):
     def create_objects(self):
         self.create_scope()
         self.create_target()
+        self.create_label()
 
     def create_scope(self):
         scope = Scope()
@@ -92,13 +103,36 @@ class Tir(Game):
         self.mouse_handlers[pygame.MOUSEBUTTONDOWN].append(scope.handle_mouse)
 
         self.scope = scope
-        self.game_objects.append(self.scope)
+        self.player.append(self.scope)
 
     def create_target(self):
-        target = Target(self.window_width, self.window_height)
+        target = Target(self.window_width, self.window_height, "DK.bmp")
         self.target = target
         # target с помощью list.insert помещаем перед объектом scope, чтоб прицел отрисовывался сверху обезьянки
-        self.game_objects.insert(0, self.target)
+        # self.game_objects.insert(0, self.target)
+        self.targets.append(self.target)
+
+    def create_label(self):
+        text_obj = TextObject(
+            0,
+            0,
+            lambda: f'Score: {self.score}',
+            (175, 185, 195),
+            'scootchover-sans.ttf',
+            24
+        )
+        self.hud_obj.append(text_obj)
+
+    def handle_shots(self):
+        for target in self.targets:
+            if self.scope.shot.colliderect(target.target_rect):
+                self.score += 10
+                # target.change_position()
+                target.get_new_coords()
+
+    def update(self):
+        self.handle_shots()
+        super().update()
 
     # def create_score(self):
     #     score = Score()
@@ -124,18 +158,25 @@ class Scope:
         self.x_scope_pos = 0
         self.y_scope_pos = 0
         self.scope_size = 20
-        # self.shot = None
         self.shoot_sound = pygame.mixer.Sound("weapons/awp.wav")
         self.shoot_sound.set_volume(0.05)
 
+        self.shot = pygame.Rect(1000, 1000, 1, 1)
+
     # Добавил ещё один аргумент event_optional, в MOUSEMOTION - это будет соответствовать event.rel,
     # а в MOUSEBUTTONDOWN - event.button
-    def handle_mouse(self, event_type, event_pos, event_optional):
+    # def handle_mouse(self, event_type, event_pos, event_optional):
+    def handle_mouse(self, event_type, event_pos):
         if event_type == pygame.MOUSEMOTION:
             self.x_scope_pos, self.y_scope_pos = event_pos
 
-        if event_type == pygame.MOUSEBUTTONDOWN and event_optional == 1:
-            self.shoot_sound.play()
+        elif event_type == pygame.MOUSEBUTTONDOWN:
+            self.shoot()
+            # self.shoot_sound.play()
+
+    def shoot(self):
+        self.shoot_sound.play()
+        self.shot = pygame.Rect(self.x_scope_pos, self.y_scope_pos, 1, 1)
 
     def blit(self, surface):
         # горизонтальная линия
@@ -157,20 +198,23 @@ class Scope:
         pass
 
     # Этим методом или свойством, я хочу передать координаты прицела во время выстрела.
-    @property
-    def shot(self):
-        return self.x_scope_pos, self.y_scope_pos
+    # @property
+    # def shot(self):
+    #     return self.x_scope_pos, self.y_scope_pos
 
 
 class Target:
-    def __init__(self, width, height) -> None:
+    def __init__(self, width, height, image) -> None:
         self.window_width = width
         self.window_height = height
 
-        self.target_img = pygame.image.load("DK.bmp")
+        # self.target_img = pygame.image.load("DK.bmp")
+        self.target_img = pygame.image.load(image)
         self.target_rect = self.target_img.get_rect()
-        self.target_rect.x = random.randint(0, self.window_width - 48)
-        self.target_rect.y = random.randint(0, self.window_height - 32)
+        # self.target_rect.x = random.randint(0, self.window_width - 48)
+        # self.target_rect.y = random.randint(0, self.window_height - 32)
+        self.target_rect.x = random.randint(0, self.window_width - self.target_rect.w)
+        self.target_rect.y = random.randint(0, self.window_height - self.target_rect.h)
 
     def blit(self, surface):
         surface.blit(self.target_img, self.target_rect)
@@ -183,13 +227,13 @@ class Target:
 
     # Этим методом я хочу изменить координаты цели, после попадания в неё
     def get_new_coords(self):
-        self.target_rect.x = random.randint(0, self.window_width - 48)
-        self.target_rect.y = random.randint(0, self.window_height - 32)
+        self.target_rect.x = random.randint(0, self.window_width - self.target_rect.w)
+        self.target_rect.y = random.randint(0, self.window_height - self.target_rect.h)
 
-    # Этим свойством я хочу передать "хитбокс" цели
-    @property
-    def get_hitbox(self):
-        return self.target_rect
+    # # Этим свойством я хочу передать "хитбокс" цели
+    # @property
+    # def get_hitbox(self):
+    #     return self.target_rect
 
 
 # class Score:
@@ -200,6 +244,27 @@ class Target:
 #     def blit(self, surface):
 #         self.score_text = self.score_obj.render(f'Score: {score}', True, (160, 200, 50))
 #         surface.blit(self.score_text, (0, 0))
+
+
+class TextObject:
+    def __init__(self, x, y, text_func, color, font_name, font_size):
+        self.x = x
+        self.y = y
+        self.text_func = text_func
+        self.color = color
+        self.font = pygame.font.Font(font_name, font_size)
+        self.text = self.get_surface(self.text_func())
+
+    def get_surface(self, text):
+        text_surface = self.font.render(text, True, self.color)
+        return text_surface, text_surface.get_rect()
+
+    def blit(self, surface):
+        text_surface, text_rect = self.get_surface(self.text_func())
+        surface.blit(text_surface, text_rect)
+
+    def update(self):
+        pass
 
 
 if __name__ == "__main__":
