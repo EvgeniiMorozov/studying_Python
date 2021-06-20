87d715b477c072785c67e94b6e419ca022de0cfa
EvgeniiMorozov <emorozov1101@gmail.com>
2021-06-20 13:35:27 +0500

    Codewars

--- main/selfedu/practice/code_wars/code_wars_3.py
+++ main/selfedu/practice/code_wars/code_wars_3.py
@@ -2,14 +2,27 @@
 def duplicate_encode(word):
     string = str()
     for i in word.lower():
-        string += "(" if word.count(i) == 1 else ")"
+        string += "(" if word.lower().count(i) == 1 else ")"
     return string
+
+
+# Task-2 Valid string - https://www.codewars.com/kata/52f3bb2095d6bfeac2002196/train/python
+
+
+def valid_word(seq, word):
+    for chunk in seq:
+        word = word.replace(chunk, "")
+    return len(word) == 0
 
 
 def main():
     # Task-1
-    print(duplicate_encode("din"))
+    # print(duplicate_encode("din"))
+
+    # Task-2
+    print(valid_word(["code", "wars"], "codewars"))
+    print(valid_word(["ab", "a", "bc"], "abc"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
