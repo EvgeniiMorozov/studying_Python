71525350d6f93c73bd70136e49852128d1a76cf7
EvgeniiMorozov <emorozov1101@gmail.com>
2021-06-21 19:54:31 +0500

    Редактирование класса клиента
    Добавлена аннотация полей класса

--- main/homeworks/homework_l36/core/client.py
+++ main/homeworks/homework_l36/core/client.py
@@ -1,8 +1,8 @@
 class Client:
     """Класс клиента"""
     def __init__(self, name, money):
-        self.name = name
-        self.money = int(money)
+        self.name: str = name
+        self.money: int = int(money)
 
     def get_money(self):
         """Возвращает текущее количество денег"""
