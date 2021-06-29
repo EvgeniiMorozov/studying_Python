d43763ef9482605072b9c792a4eb98da1ad6235f
Evgenii Morozov <zheka1101@yandex.ru>
2021-06-29 09:57:07 +0500

    Добавленны функции для решения первого задания ДЗ

--- main/homeworks/homework_l38/homework_l38.py
+++ main/homeworks/homework_l38/homework_l38.py
@@ -14,3 +14,34 @@
 
 Нюансы реализации разрешаются программистом.
 """
+
+import concurrent.futures as cf
+from random import randint
+
+
+# Task-1
+
+def generate_rand_nums(length: int):
+    """Генерирует список со случайными числами от 1 до 1000 заданной длины length"""
+    return [randint(1, 1000) for _ in range(length)]
+
+
+def multithread_work():
+    with cf.ThreadPoolExecutor(max_workers=3) as executor:
+        future1 = executor.submit(generate_rand_nums, 30)
+        future2 = executor.submit(lambda lst: sum(lst)/len(lst), future1.result())
+        future3 = executor.submit(sum, future1.result())
+    print(f"Сгенерированный список: {future1.result()}")
+    print(f"Среднее арифметическое значение списка: {future2.result()}")
+    print(f"Сумма всех членов списка: {future3.result()}")
+
+
+def main():
+
+    # Task-1
+    # print(generate_rand_nums(30))
+    multithread_work()
+
+
+if __name__ == '__main__':
+    main()
