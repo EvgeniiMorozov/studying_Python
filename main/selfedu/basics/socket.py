87762771f3ef84300380ec0169c02eb460df8d07
Evgenii Morozov <zheka1101@yandex.ru>
2021-07-07 17:11:52 +0500

    selfeducation
    socket

--- main/selfedu/basics/socket.py
+++ main/selfedu/basics/socket.py
@@ -1,21 +1,54 @@
 import socket
 
 
+URLS = {
+    "/": "hello index",
+    "/blog": "hello blog",
+}
+
+
+def parse_request(request):
+    parsed = request.split(" ")
+    method = parsed[0]
+    url = parsed[1]
+    return (method, url)
+
+
+def generate_headers(method, url):
+    if method != "GET":
+        return ("HTTP/1.1 405 Method not allowed\n\n", 405)
+
+    if url not in URLS:
+        return ("HTTP/1.1 404 Not found\n\n", 404)
+
+    return ("HTTP/1.1 200 OK\n\n", 200)
+
+
+def generate_response(request):
+    method, url = parse_request(request)
+    headers, code = generate_headers(method, url)
+    return (headers + "hello world").encode()
+
+
 def run():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind('localhost', 5000)
+    # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket = socket.socket()
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind("localhost", 5000)
     server_socket.listen()
 
     while True:
         client_socket, addr = server_socket.accept()
         request = client_socket.recv(1024)
         print(request)
-        print(f'\n{addr}')
+        print()
+        print(f"\n{addr}")
 
-        client_socket.sendall('hello world'.encode())
+        response = generate_response(request.decode("utf-8"))
+
+        client_socket.sendall(response)
         client_socket.close()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
