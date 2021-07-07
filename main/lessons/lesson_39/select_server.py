26248701771bcb9fac30e275e47baec22afabe1d
EvgeniiMorozov <emorozov1101@gmail.com>
2021-07-07 21:20:25 +0500

    Урок 3.07.2021

--- main/lessons/lesson_39/select_server.py
+++ main/lessons/lesson_39/select_server.py
@@ -4,29 +4,29 @@ from select import select
 to_monitor = []
 
 server_socket = socket.socket()
-server_socket.bind(('localhost', 5000))  # 127.0.0.1:5000
+server_socket.bind(("localhost", 5000))  # 127.0.0.1:5000
 server_socket.listen()
 
 
 def accept_connection(server_socket):
-    print('Перед подключением')
+    print("Перед подключением")
     client_socket, addr = server_socket.accept()
-    print(f'Подключился: {addr}')
+    print(f"Подключился: {addr}")
     to_monitor.append(client_socket)
 
 
 def send_message(client_socket):
     try:
-        print(f'Ждём сообщение от {client_socket.getpeername()}')
+        print(f"Ждём сообщение от {client_socket.getpeername()}")
         request = client_socket.recv(1024)
-        if request is None or request == '' or request == ' ':
+        if request is None or request == "" or request == " ":
             to_monitor.remove(client_socket)
             client_socket.close()
     except socket.error:
         to_monitor.remove(client_socket)
         client_socket.close()
     else:
-        print(f'{client_socket.getpeername()} => {request.decode()}')
+        print(f"{client_socket.getpeername()} => {request.decode()}")
         client_socket.send(request)
 
 
