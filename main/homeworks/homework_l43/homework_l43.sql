f0379b205a08f015efb2d9a1a2fed1e6808be3da
Evgenii Morozov <zheka1101@yandex.ru>
2021-07-27 09:01:40 +0500

    homework solution

--- main/homeworks/homework_l43/homework_l43.sql
+++ main/homeworks/homework_l43/homework_l43.sql
@@ -1,8 +1,6 @@
-SELECT
-    department,
+SELECT department,
     MAX(salary) AS max_salary,
     AVG(salary) AS average_salary,
     MIN(salary) AS min_salary
-FROM
-    `shop`.`employee`
+FROM `shop`.`employee`
 GROUP BY department;
