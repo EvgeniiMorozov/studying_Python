4647f5ad9acce80eac135bc59ca16dc22f392c03
Evgenii Morozov <zheka1101@yandex.ru>
2021-08-04 13:47:51 +0500

    homework solution

--- main/homeworks/homework_l46/homework_l46.py
+++ main/homeworks/homework_l46/homework_l46.py
@@ -16,10 +16,84 @@
 """
 
 import mysql.connector
+from random import choice, randint
+from sqlalchemy.engine import create_engine
+from sqlalchemy.sql.schema import Column, MetaData, Table
+from sqlalchemy.sql.sqltypes import Integer, String
+
+
+def create_database():
+    db_connection = mysql.connector.connect(host="localhost", user="root", password="12345")
+    db_cursor = db_connection.cursor()
+    db_cursor.execute("CREATE DATABASE players")
+    db_cursor.close()
+    db_connection.close()
+
+
+def create_table(metadata, engine):
+    player = Table(
+        "player",
+        metadata,
+        Column("id", Integer(), primary_key=True),
+        Column("nickname", String(45), nullable=False, unique=True),
+        Column("role", String(45), nullable=False),
+        Column("level", Integer(), nullable=False),
+        Column("strenght", Integer(), nullable=False),
+        Column("dexterity", Integer(), nullable=False),
+        Column("mind", Integer(), nullable=False),
+    )
+
+    metadata.create_all(engine)
+
+    return player
+
+
+def create_player() -> tuple:
+    nicknames = [
+        "David",
+        "Robert",
+        "Mario",
+        "Lawrence",
+        "Anita",
+        "Carlson",
+        "Cedric",
+        "Ortega",
+        "Elliot",
+        "Robles",
+        "Emilio",
+        "May",
+        "Elisabeth",
+        "Riley",
+        "Robyn",
+        "Contreras",
+        "Sean",
+        "Robbin",
+        "Ashlee",
+        "Kelly",
+    ]
+    roles = ["wizard", "warrior", "priest", "dark knight", "archer"]
+    level = randint(1, 30)
+    strenght = randint(5, 15)
+    dexterity = randint(5, 15)
+    mind = randint(5, 15)
+    for name in nicknames:
+        yield (name, choice(roles), level, strenght, dexterity, mind)
 
 
 def main():
-    pass
+    create_database()
+
+    engine = create_engine("mysql+mysqlconnector://root:12345@localhost/players", echo=True)
+    metadata = MetaData()
+
+    conn = engine.connect()
+
+    players = create_table(metadata, engine)
+
+    for _ in range(20):
+        player_data = players.insert().values(*create_player)
+        conn.execute(player_data)
+        print(player_data.nickname)
 
 
 if __name__ == "__main__":
